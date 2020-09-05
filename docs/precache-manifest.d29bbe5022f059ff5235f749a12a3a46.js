9ad77784fc6cd08fd0d7606ab2f5b7987ffd0068
Oscar Hernández <dobok1@gmail.com>
2020-09-04 20:27:16 -0500

    Docs agregados

---
+++ docs/precache-manifest.d29bbe5022f059ff5235f749a12a3a46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d16b914ec23cffb8242964e2d57864",
+    "url": "/index.html"
+  },
+  {
+    "revision": "536054dc32a6a7ea7fbb",
+    "url": "/static/css/main.0e80e6d7.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536054dc32a6a7ea7fbb",
+    "url": "/static/js/main.07b6fdaf.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
